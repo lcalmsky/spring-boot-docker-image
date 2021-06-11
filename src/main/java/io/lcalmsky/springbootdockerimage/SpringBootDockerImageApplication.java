b2d6b5a7f728b0fbfde671ed6979946166ce4271
이정민(Jungmin Lee)/Payment개발팀/11ST <jaime.lee@sk.com>
2021-06-11 16:34:52 +0900

    Initial commit

---
+++ src/main/java/io/lcalmsky/springbootdockerimage/SpringBootDockerImageApplication.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.springbootdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerImageApplication.class, args);
+    }
+
+}
